766ea16f69176c051d276cb03c8ba237619023e3
Simon <simonaspuceika@gmail.com>
2026-01-30 10:15:54 +0100

    02 / Subklassen überschreiben

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,7 +4,23 @@ public class App {
     
     public static void main(String[] args) {
      
-        
+      Queen queen = new Queen();
+        Worker worker = new Worker();
+        Drone drone = new Drone();   
+
+        /* 1. Iteration  */
+
+        output(queen.doYourJob());
+        output(queen.fly());
+        output("-------------------");
+
+        output(worker.doYourJob());
+        output(worker.fly());
+        output("-------------------");
+
+        output(drone.doYourJob());
+        output(drone.fly());
+        output("-------------------");	  
     }
 
    
